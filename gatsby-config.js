5566c69b20bed1538d84022ac9671ed31bf7b1db
Ward Peeters <ward@coding-tech.com>
2020-06-23 09:46:02 +0200

    setup perf recipes

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,18 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
-
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-route-dictionary",
+    "gatsby-disable-prefetch",
+    {
+      resolve: "gatsby-plugin-webpack-bundle-analyser-v2",
+      options: {
+        analyzerMode: "static",
+        reportFilename: "_bundle.html",
+        openAnalyzer: false,
+      },
+    },
+  ],
 }
